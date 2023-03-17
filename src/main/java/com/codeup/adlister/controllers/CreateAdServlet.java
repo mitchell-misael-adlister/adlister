c0b94f6cd7354d02af89172918c5b620122d0ef0
Misael Cuadrado <104469677+nelsonymarie@users.noreply.github.com>
2023-03-17 16:27:23 -0500

    StickyFormsUpdate

--- src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
@@ -2,7 +2,6 @@ package com.codeup.adlister.controllers;
 
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.Ad;
-import com.codeup.adlister.models.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,20 +13,30 @@ import java.io.IOException;
 @WebServlet(name = "controllers.CreateAdServlet", urlPatterns = "/ads/create")
 public class CreateAdServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (request.getSession().getAttribute("user") == null) {
-            response.sendRedirect("/login");
-            return;
-        }
         request.getRequestDispatcher("/WEB-INF/ads/create.jsp")
-            .forward(request, response);
+                .forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        User user = (User) request.getSession().getAttribute("user");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        // Extract the title and description from the request
+        String adTitle = request.getParameter("title");
+        String adDescription = request.getParameter("description");
+
+        // Check if title or description is empty
+        if (adTitle.isEmpty() || adDescription.isEmpty()) {
+            // If either field is empty, set the attributes in the request
+            request.setAttribute("adTitle", adTitle);
+            request.setAttribute("adDescription", adDescription);
+            // Forward the request back to the create.jsp page with the populated fields
+            request.getRequestDispatcher("/WEB-INF/ads/create.jsp").forward(request, response);
+            return;
+        }
+
+        // Create and save the ad if both fields
         Ad ad = new Ad(
-            user.getId(),
-            request.getParameter("title"),
-            request.getParameter("description")
+                1, //  the user id
+                adTitle,
+                adDescription
         );
         DaoFactory.getAdsDao().insert(ad);
         response.sendRedirect("/ads");
