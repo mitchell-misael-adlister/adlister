797abe971e962c97a06b11c9611103b140ed1a0a
mcampbell379 <campbell.mitchell45@gmail.com>
2023-03-17 10:39:39 -0500

    finishing adlister

---
+++ src/main/java/com/codeup/adlister/dao/Config.java
@@ -0,0 +1,17 @@
+package com.codeup.adlister.dao;
+
+
+public class Config {
+
+    public String getUrl() {
+        return "jdbc:mysql://studentdb.fulgentcorp.com/ymir_mitchell?allowPublicKeyRetrieval=true&useSSL=false";
+    }
+
+    public String getUser() {
+        return "ymir_mitchell";
+    }
+
+    public String getPassword() {
+        return "vklGnle859AzjlD";
+    }
+}
